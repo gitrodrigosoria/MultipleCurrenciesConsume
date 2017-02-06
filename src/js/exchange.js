08c40c1f109e2b3933eaa747443613b1a18bfc5a
Rodrigo Sória <gitrodrigosoria@users.noreply.github.com>
2017-02-06 13:21:05 -0200

    Update exchange.js

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,27 +1,26 @@
-getExchange = function () {
+getExchange = function() {
 
-    var urlYql = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%3D%22EURBRL%22&"
-            + "format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    var urlYql = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%3D%22EURBRL%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
-    var urlApi = "http://rodrigosoriabr-001-site1.etempurl.com/Service.ashx"
+    var urlApi = "../euro/service.ashx"
 
     var euro, euro_iof, agk, spmundi;
 
-    $.getJSON(urlYql, function (d) {
+    $.getJSON(urlYql, function(d) {
 
         euro = parseFloat(d.query.results.rate.Rate);
-        
+
         euro_iof = eval(euro + eval(euro * 0.0038));
 
         $('#euro_value').html(
             "EURO: " + euro.toFixed(2) + " <span>(" + euro.toFixed(4) + ") :: (" + euro_iof.toFixed(4) + ")</span><br/><hr/>"
         );
 
-        $.getJSON(urlApi, function (d) {
+        $.getJSON(urlApi, function(d) {
 
-            agk = d.Cotacao[0].AGK[0][6].Value.substring(0, 4).replace(',', '.');
-            
-            spmundi = d.Cotacao[1].SPMUNDI[0][7].Value.replace(',', '.');
+            agk = d.Cotacao[0].AGK[0][5].Value.substring(0, 4).replace(',', '.');
+
+            spmundi = d.Cotacao[1].SPMUNDI[0][3].Value.replace(',', '.');
 
             $('#agk_euro_value').html(
                 "AGK: " + agk +
@@ -42,4 +41,6 @@ getExchange = function () {
 
 getExchange();
 
-setInterval(function () { getExchange(); }, 10000);
+setInterval(function() {
+    getExchange();
+}, 10000);
